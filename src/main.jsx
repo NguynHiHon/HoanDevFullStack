7190fd733e6f1070d68524c2d2ea720ee901aadc
NguynHiHon <nguyen.h.hoan03@gmail.com>
2025-08-09 14:09:48 +0700

    update eslint mui config and rule

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-
+import CssBaseline from '@mui/material/CssBaseline'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline /> {/* Nhất quán trên các trình duyệt khác nhau */}
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
